4d10d8460e98cb668b06762e3de607b6c7c8b801
Ras <contact@rasputin.io>
2016-11-11 19:12:11 -0800

    Add server code

---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+import logger from './logger.js'
+
+const app = express()
+
+const port = process.env.PORT || 5000
+
+logger.info('Booting up...')
+
+app.use(logger.middleware)
+
+app.get('/', (req, res) => {
+	res.send('Hello world!')
+})
+
+app.listen(port, () => {
+	logger.info('Server listening on port %s...', port)
+})
