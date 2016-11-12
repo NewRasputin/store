db2ef7ef7a3863d9eed3c66ca7f818db937bbf27
Ras <contact@rasputin.io>
2016-11-11 19:54:09 -0800

    Add varsIgnorePattern

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,9 @@ import express from 'express'
 
 import logger from './logger.js'
 
-import db from './db/config.js'
+import _db from './db/config.js'
+
+import api from './routes/api.js'
 
 const app = express()
 
@@ -11,6 +13,8 @@ const port = process.env.PORT || 5000
 logger.info('Booting up...')
 
 app.use(logger.middleware)
+
+app.use('/api', api)
 
 app.get('/', (req, res) => {
 	res.send('Hello world!')
