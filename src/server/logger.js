a9ad090b5ff19df86174a8549ed5735022acb32a
Ras <contact@rasputin.io>
2016-11-11 17:06:48 -0800

    Add logger middleware

--- src/server/logger.js
+++ src/server/logger.js
@@ -4,5 +4,11 @@ const logger = {}
 logger.info = debug('app:info')
 logger.warn = debug('app:warn')
 logger.error = debug('app:error')
+logger.httpinf = debug('http:info')
+logger.httperr = debug('http:error')
+logger.middleware = (req, res, next) => {
+	logger.httpinf('%s %s', req.method, req.url)
+	next()
+}
 
 export default logger
