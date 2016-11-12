c060f425d1125e3bf370c55b2cb2799e8a820fc5
Ras <contact@rasputin.io>
2016-11-11 16:32:34 -0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp')
+var nodemon = require('gulp-nodemon')
+var eslint = require('gulp-eslint')
+var babel = require('gulp-babel')
+
+gulp.task('start', () => {
+  nodemon({
+    script: 'dist/server.js',
+    env: {'NODE_ENV': 'development', 'DEBUG': 'app:*'}
+  })
+})
+
+gulp.task('server', () => {
+  gulp.src('src/server/**/*.*')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch('src/server/**/*.*', ['server'])
+})
+
+gulp.task('dev', ['start', 'watch'])
