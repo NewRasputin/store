adc692d5fe27232cfa7a09224d44578c76b91460
Ras <contact@rasputin.io>
2016-11-12 11:13:29 -0800

    Add mongoose promise

--- src/server/db/config.js
+++ src/server/db/config.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import logger from '../logger.js'
 
+mongoose.Promise = global.Promise
+
 mongoose.connect('mongodb://localhost/store', err => {
 	if (err) {
 		logger.error(err)
