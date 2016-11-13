1e17132c3b154e0b2136c5e6f8ac3d3fd32e41c0
Ras <contact@rasputin.io>
2016-11-13 12:08:48 -0800

    Add NavBar component

---
+++ src/client/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="">
+		<router-link to="/">Home</router-link>
+		<router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
