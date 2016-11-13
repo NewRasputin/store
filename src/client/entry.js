035788074e650e2604731d29ef4bae0ecb4a56f0
Ras <contact@rasputin.io>
2016-11-13 12:07:42 -0800

    Add router

--- src/client/entry.js
+++ src/client/entry.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 
 import App from './App.vue'
+import Home from './components/pages/Home.vue'
+import About from './components/pages/About.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+	routes: [
+		{path: '/', component: Home},
+		{path: '/about', component: About}
+	]
+})
 
 new Vue({
 	el: '#app',
+	router,
 	components: {App},
 	render: h => h(App)
 })
