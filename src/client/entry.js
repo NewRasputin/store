964f312ab302b7e4861421b79278b7a03c8086bc
Ras <contact@rasputin.io>
2016-11-14 12:07:27 -0800

    Add Vuex store

--- src/client/entry.js
+++ src/client/entry.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import Vuex from 'vuex'
 
 import App from './App.vue'
 import Home from './components/pages/Home.vue'
@@ -8,6 +9,18 @@ import About from './components/pages/About.vue'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	state: {
+		items: []
+	},
+	mutations: {
+		setItems (state, items) {
+			state.items = items
+		}
+	}
+})
 
 const router = new VueRouter({
 	routes: [
@@ -19,6 +32,7 @@ const router = new VueRouter({
 new Vue({
 	el: '#app',
 	router,
+	store,
 	components: {App},
 	render: h => h(App)
 })
