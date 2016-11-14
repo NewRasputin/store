5e79b4226c368935d8c31f24957837b14bcbf30e
Ras <contact@rasputin.io>
2016-11-14 12:59:18 -0800

    Add style

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,21 +1,30 @@
 <template lang="html">
-	<div>
+	<div class="app">
 		<NavBar></NavBar>
-		<router-view></router-view>
+		<div class="container">
+			<router-view></router-view>
+		</div>
 	</div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
 export default {
+	name: 'App',
 	components: {NavBar}
 }
 </script>
 
 <style lang="css">
 	@import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto+Slab');
-	html, body {
+	html, body, .app {
 		margin: 0;
+		height: 100%;
 		font-family: 'Open Sans', sans-serif;
+	}
+	.container {
+		padding: 3rem;
+		background-color: #F5F5F5;
+		height: 100%;
 	}
 </style>
