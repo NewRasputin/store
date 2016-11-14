e5f3e7f1706a849a9c61864b6e0cd8f909802358
Ras <contact@rasputin.io>
2016-11-14 14:05:03 -0800

    Display items on homepage

--- src/client/components/pages/Home.vue
+++ src/client/components/pages/Home.vue
@@ -1,13 +1,41 @@
 <template lang="html">
-  <div>
-  	oh hey there
+  <div class="flex-container">
+		<Item
+			v-for='(item, index) in items'
+			v-bind:item="item"
+			v-bind:index="index">
+		</Item>
   </div>
 </template>
 
 <script>
+import Item from '../Item.vue'
 export default {
+	name: 'Home',
+	components: {Item},
+	computed: {
+		items () {
+			return this.$store.state.items
+		}
+	},
+	mounted () {
+		this.$http.get('/api/items')
+			.then((res) => {
+				this.$store.commit('setItems', res.body)
+			}, (res) => {
+				console.log(res.body)
+			})
+	}
 }
 </script>
 
 <style lang="css">
+img {
+	max-width: 400px;
+}
+.flex-container {
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: space-around;
+}
 </style>
