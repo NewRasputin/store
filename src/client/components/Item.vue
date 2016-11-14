0e0bb859c9ea523e0e30dc0f2f8eb15e31a4a1d2
Ras <contact@rasputin.io>
2016-11-14 13:00:40 -0800

    Add Item component

---
+++ src/client/components/Item.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="">
+		<img src="https://bytesizemoments.com/wp-content/uploads/2014/04/placeholder3.png" alt="" />
+		<h3>{{item.name}}</h3>
+		<p>{{item.desc}} - ${{item.price.toFixed(2)}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Item',
+	props: ['item']
+}
+</script>
+
+<style lang="css">
+</style>
