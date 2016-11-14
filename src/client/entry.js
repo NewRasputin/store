c3cc1b5d2f906b6c031506b2129941a79e37f7f6
Ras <contact@rasputin.io>
2016-11-14 10:55:38 -0800

    Add Vue resource

--- src/client/entry.js
+++ src/client/entry.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 import App from './App.vue'
 import Home from './components/pages/Home.vue'
 import About from './components/pages/About.vue'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 const router = new VueRouter({
 	routes: [
