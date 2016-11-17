49a4832bce4c8e0c9c0758008c6694981a2c83ef
Ras <contact@rasputin.io>
2016-11-16 16:46:12 -0800

    Add modal and check for session

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,6 +1,11 @@
 <template lang="html">
 	<div class="app">
 		<NavBar></NavBar>
+		<transition name="fade">
+			<Modal v-if="showLogin">
+				<LoginForm />
+			</Modal>
+		</transition>
 		<div class="container">
 			<router-view></router-view>
 		</div>
@@ -9,9 +14,24 @@
 
 <script>
 import NavBar from './components/NavBar.vue'
+import Modal from './components/Modal.vue'
+import LoginForm from './components/LoginForm.vue'
 export default {
 	name: 'App',
-	components: {NavBar}
+	components: {NavBar, Modal, LoginForm},
+	created () {
+		this.$http.get('/auth/login')
+			.then((res) => {
+				this.$store.state.username = res.body.username
+			}, (res) => {
+
+			})
+	},
+	computed: {
+		showLogin () {
+			return this.$store.state.showLogin
+		}
+	}
 }
 </script>
 
@@ -21,10 +41,17 @@ export default {
 		margin: 0;
 		height: 100%;
 		font-family: 'Open Sans', sans-serif;
+		overflow: hidden;
 	}
 	.container {
 		padding: 3rem;
 		background-color: #F5F5F5;
 		height: 100%;
+	}
+	.fade-enter-active, .fade-leave-active {
+  	transition: opacity .5s ease;
+	}
+	.fade-enter, .fade-leave-active {
+	  opacity: 0;
 	}
 </style>
