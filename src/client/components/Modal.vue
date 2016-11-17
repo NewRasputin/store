3d0fea8579a5d8fb6e5a4ab75e7c441367e1db60
Ras <contact@rasputin.io>
2016-11-16 16:41:12 -0800

    Add modal component

---
+++ src/client/components/Modal.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class='modalWrap'>
+		<div @click="toggleModal" class="modal">
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Modal',
+	methods: {
+		toggleModal () {
+			this.$store.commit('toggleModal')
+		}
+	}
+}
+</script>
+
+<style lang="css" scoped>
+</style>
