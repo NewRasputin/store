8d6cea5f61faf83bad298f02bd882844ada6615d
Ras <contact@rasputin.io>
2016-11-16 16:41:45 -0800

    Fix indentation

--- src/client/components/Modal.vue
+++ src/client/components/Modal.vue
@@ -1,6 +1,7 @@
 <template lang="html">
   <div class='modalWrap'>
 		<div @click="toggleModal" class="modal">
+			<slot>If you can see this I think that means something went wrong...</slot>
 		</div>
 	</div>
 </template>
@@ -17,4 +18,18 @@ export default {
 </script>
 
 <style lang="css" scoped>
+	.modalWrap {
+		z-index: 1;
+		display: table;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+	.modal {
+		display: table-cell;
+		vertical-align: middle;
+		background-color: rgba(15,15,15,0.75) ;
+	}
 </style>
